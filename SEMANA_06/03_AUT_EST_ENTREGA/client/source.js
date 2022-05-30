9cc29d56cf8cb44b8e98e8fa01b1af6d8d089c02
Luiz Granville <58540177+Luiz-Granville@users.noreply.github.com>
2022-05-29 21:20:42 -0300

    Atualização de atividade obrigatória

---
+++ SEMANA_06/03_AUT_EST_ENTREGA/client/source.js
@@ -0,0 +1,21 @@
+function createLine(){
+    $("#alert-message")[0].innerHTML = "Você ira receber informações no seu e-mail!"
+}
+
+function getData(){
+    let url = "/information";
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+    let data = JSON.parse(xhttp.responseText)[0]
+    
+    $(".nameTitle")[0].innerHTML = data.name
+    $("#tellphone")[0].innerHTML = data.tellphone
+    $("#email")[0].innerHTML = data.email
+    $("#site")[0].innerHTML = data.site
+    $("#github")[0].innerHTML = data.github
+    $("#linkedin")[0].innerHTML = data.linkedin
+}
+
+getData()
